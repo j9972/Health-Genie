dad88ed98b747e3aa8b6e9bc422b14e7b3bdde8f
jung su young <j9972@naver.com>
2023-05-16 23:18:36 +0900

    first commit

---
+++ src/main/java/com/example/healthgenie/HealthGenieApplication.java
@@ -0,0 +1,13 @@
+package com.example.healthgenie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthGenieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthGenieApplication.class, args);
+    }
+
+}
