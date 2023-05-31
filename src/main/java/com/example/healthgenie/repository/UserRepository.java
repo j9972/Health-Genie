8827d027bcbff2e2df1cb481ad1952a74d8fcad4
jung su young <j9972@naver.com>
2023-05-31 22:58:14 +0900

    수정, 생성: update docker file, write findByRoleAndId() in the userRepository

--- src/main/java/com/example/healthgenie/repository/UserRepository.java
+++ src/main/java/com/example/healthgenie/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.example.healthgenie.repository;
 
+import com.example.healthgenie.entity.Role;
 import com.example.healthgenie.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     // user_tb findByEmailId(@Param("email") String email);
+
+    User findByRoleAndId(Role role, Long Id);
 }
