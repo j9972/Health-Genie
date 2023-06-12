bec11e94b390c757ae1690523600391b658b182a
jeaun80 <kims1706@naver.com>
2023-06-12 19:51:28 +0900

    fix : 회원가입-이메일인증-로그인 기능 로직 수정

--- src/main/java/com/example/healthgenie/dto/userRegisterDto.java
+++ src/main/java/com/example/healthgenie/dto/userRegisterDto.java
@@ -1,9 +1,15 @@
 package com.example.healthgenie.dto;
 
 import com.example.healthgenie.entity.Role;
+import com.example.healthgenie.entity.User;
 import lombok.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Collections;
 
 @Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -13,4 +19,26 @@ public class userRegisterDto {
     private String name;
     private String uniName;
     private Role role;
+    private String provider;
+
+
+    public User toEntity(PasswordEncoder passwordEncoder) {
+        return User.builder()
+                .email(email)
+                .password(passwordEncoder.encode(password))
+                .uniName(uniName)
+                .name(name)
+                .role(role)
+                .build();
+    }
+
+    public User toEntity() {
+        return User.builder()
+                .email(email)
+                .uniName(uniName)
+                .name(name)
+                .provider(provider)
+                .role(role)
+                .build();
+    }
 }
