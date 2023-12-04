1ca743d88ebe025bb673936aa27862e67fe723f1
김경렬 <rlarudfuf802@naver.com>
2023-12-04 16:13:25 +0900

    feat: 매칭 개발 잠시 중단

--- src/main/java/com/example/healthgenie/boundedContext/matching/entity/Matching.java
+++ src/main/java/com/example/healthgenie/boundedContext/matching/entity/Matching.java
@@ -1,14 +1,14 @@
 package com.example.healthgenie.boundedContext.matching.entity;
 
-import com.example.healthgenie.boundedContext.user.entity.User;
 import com.example.healthgenie.base.entity.BaseEntity;
+import com.example.healthgenie.boundedContext.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.sql.Date;
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
@@ -24,33 +24,25 @@ public class Matching extends BaseEntity {
     private Long id;
 
     @Column(name = "pt_date")
-    private Date date; // LocalDateTime으로 통합 필요 -> 원하는 데이터로 프론트에서 파싱하든 DTO로 반환해서 주든 해야할듯?
+    private LocalDateTime date;
 
     @Column(name = "pt_place")
     private String place;
 
-    @Column(name = "pt_time")
-    private String time; // LocalDateTime으로 통합 필요
-
     @Column(name = "pt_accept")
-    private boolean ptAccept; // 필요할까? PT가 수락 됐을 경우에만 매칭 데이터를 생성하는게 맞지 않나? 애초에 거절한 내역을 DB에 저장할 필요가 없어보임. 만약 요구사항이 미래에 거절한 내역이 필요할 것 같아서 추가했다 라고 한다면 필요 -> 그렇다면 변수 네이밍 수정 ex: isAccepted
+    private boolean isAccepted; // PT 최종 수락/취소 필드
 
     @Column(name = "pt_price")
-    private int price; // PT 가격은 어떻게 결정되는가? 애초부터 가격정찰제로 딱 정해놓던지, 채팅방에서 트레이너가 매칭 요청을 보낼 때 채팅으로 협의를 끝마친 가격으로 보일 수 있도록 디자인 수정이 필요하지 않을까?
+    private int price;
 
-    /*
-        [채팅에서] 트레이너가 PT 요청시 회원이 수락 하면 True값으로 변경, 취소하면 default 값 그대로 유지
-     */
-    @Column(name = "pt_experiecne")
-    private boolean expericence; // ptAccept와 같은 맥락으로 고민해봐야할듯?
+    @Column(name = "pt_description")
+    private String description;
 
     @ManyToOne
     @JoinColumn(name ="user_id")
-    private User member;
+    private User user;
 
     @ManyToOne
     @JoinColumn(name ="trainer_id")
     private User trainer;
-
-    // PT 매칭 데이터에 "내용" 필드가 추가되어야 할 것 같음 -> 디자인에는 "내용" 필드 존재
 }
--- src/main/java/com/example/healthgenie/boundedContext/matching/service/MatchingService.java
+++ src/main/java/com/example/healthgenie/boundedContext/matching/service/MatchingService.java
@@ -4,10 +4,12 @@ import com.example.healthgenie.boundedContext.matching.repository.MatchingReposi
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
+@Transactional(readOnly = true)
 public class MatchingService {
 
     private final MatchingRepository matchingRepository;
