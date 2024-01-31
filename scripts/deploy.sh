6d73ec5612b95c20a5cea8ce6fd51202248a0737
j9972 <j9972@naver.com>
2024-01-31 16:21:55 +0900

    test : test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -26,11 +26,13 @@ echo "> 컨테이너 ID: ${CONTAINER_ID}" >> /home/ubuntu/cicd/start.log
 if [ -z ${CONTAINER_ID} ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/cicd/deploy.log
+  echo "> if 안 ----------------" >> /home/ubuntu/cicd/start.log
 else
   echo "> docker stop ${CONTAINER_ID}"
   sudo docker stop ${CONTAINER_ID}
   echo "> docker rm ${CONTAINER_ID}"
   sudo docker rm ${CONTAINER_ID}
+  echo "> else 안 ----------------" >> /home/ubuntu/cicd/start.log
   sleep 5
 fi
 
