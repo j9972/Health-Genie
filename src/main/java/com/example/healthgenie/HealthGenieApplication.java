5c7efffdcd6e5ff409f86239db0509f2f7510795
j9972 <j9972@naver.com>
2024-01-31 15:32:07 +0900

    test : test

--- src/main/java/com/example/healthgenie/HealthGenieApplication.java
+++ src/main/java/com/example/healthgenie/HealthGenieApplication.java
@@ -1,15 +1,18 @@
 package com.example.healthgenie;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@Slf4j
 public class HealthGenieApplication {
 
     public static void main(String[] args) {
+
+        log.info("test - deploy---------------------------------------------");
         SpringApplication.run(HealthGenieApplication.class, args);
     }
-
 }
