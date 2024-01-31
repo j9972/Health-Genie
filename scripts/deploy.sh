4bf70aff6fb16e881cd3b7d268226fc8efa19c65
j9972 <j9972@naver.com>
2024-01-31 16:52:01 +0900

    docs : sh에 주석 처리

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,23 +5,23 @@ NOW=$(date +%c)
 
 CONTAINER_ID=$(docker container ls -f "name=health_genie_1" -q)
 
-echo "[$NOW] > 컨테이너 ID: ${CONTAINER_ID}" >> /home/ubuntu/cicd/start.log
+echo "[$NOW] > 컨테이너 ID: ${CONTAINER_ID}" >> /home/ubuntu/cicd/deploy.log
 
 if [ -z ${CONTAINER_ID} ]
 then
   echo "[$NOW] > 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/cicd/deploy.log
 else
-  echo "[$NOW] > docker stop ${CONTAINER_ID}"
+  echo "[$NOW] > docker stop ${CONTAINER_ID}" >> /home/ubuntu/cicd/deploy.log
   sudo docker stop ${CONTAINER_ID}
-  echo "[$NOW] > docker rm ${CONTAINER_ID}"
+  echo "[$NOW] > docker rm ${CONTAINER_ID}" >> /home/ubuntu/cicd/deploy.log
   sudo docker rm ${CONTAINER_ID}
-  echo "[$NOW] > docker rmi -f j9972/health_genie "
+  echo "[$NOW] > docker rmi -f j9972/health_genie " >> /home/ubuntu/cicd/deploy.log
   sudo docker rmi -f j9972/health_genie
   sleep 5
 fi
 
 export ENCRYPTED_PASSWORD_FILE="/home/ubuntu/cicd/jasypt.txt"
-#cd /home/ubuntu/cicd && docker build -t yapp .
+
 docker run \
   --name=health_genie_1 \
   --restart unless-stopped \
@@ -32,4 +32,4 @@ docker run \
   --net mybridge \
   j9972/health_genie
 
-echo "[$NOW] > health genie server start!! welcome to Health- Genie "
+echo "[$NOW] > health genie server start!! welcome to Health- Genie " >> /home/ubuntu/cicd/deploy.log
