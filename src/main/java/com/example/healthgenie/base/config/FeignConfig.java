4c1bd8e6a4e00399268318b1fb39b4c628f9579e
김경렬 <rlarudfuf802@naver.com>
2024-02-16 17:44:51 +0900

    feat: OpenFeign 설정

---
+++ src/main/java/com/example/healthgenie/base/config/FeignConfig.java
@@ -0,0 +1,14 @@
+package com.example.healthgenie.base.config;
+
+import feign.Client;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    public Client feignClient() {
+        return new Client.Default(null, null);
+    }
+}
