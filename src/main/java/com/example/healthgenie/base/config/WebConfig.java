d1952fb30030362267c2015bd6156eef7c2cd77b
j9972 <j9972@naver.com>
2024-02-27 11:01:21 +0900

    docs : 주석 추가

--- src/main/java/com/example/healthgenie/base/config/WebConfig.java
+++ src/main/java/com/example/healthgenie/base/config/WebConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     // CORS는 프론트엔드, 백엔드개발자라면 반드시 이해!
-    // CORS에 대한 설정. CORS는 Cross Origin Resource Sharing의 약자.
+    // CORS에 대한 설정. CORS는 Cross Origin Resource Sharing의 약자. -> 3000번 포트 추가해야함
     // 프론트 엔드, 백 엔드 개발
     // 프론트 엔드는 3000번 포트 (React.js), 백 엔드는 8080번 포트
     // http://localhost:3000 ---> 8080 api를 호출할 수 있도록 설정.
