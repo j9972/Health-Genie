e986a0e30f5b5811eb008ab46fc25918422e6e95
김경렬 <rlarudfuf802@naver.com>
2024-02-28 23:34:46 +0900

    refactor: LikeController RequestMapping 변경

--- src/main/java/com/example/healthgenie/boundedContext/community/like/controller/LikeController.java
+++ src/main/java/com/example/healthgenie/boundedContext/community/like/controller/LikeController.java
@@ -11,7 +11,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/likes/{postId}")
+@RequestMapping("/community/{postId}/likes")
 @RequiredArgsConstructor
 public class LikeController {
 
