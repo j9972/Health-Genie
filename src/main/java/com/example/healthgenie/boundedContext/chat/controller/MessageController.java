3d32b1f3aa144e151b82ef66543fd342dad71168
김경렬 <rlarudfuf802@naver.com>
2024-03-06 01:31:10 +0900

    chore: 수정

--- src/main/java/com/example/healthgenie/boundedContext/chat/controller/MessageController.java
+++ src/main/java/com/example/healthgenie/boundedContext/chat/controller/MessageController.java
@@ -3,7 +3,6 @@ package com.example.healthgenie.boundedContext.chat.controller;
 import com.example.healthgenie.base.response.Result;
 import com.example.healthgenie.boundedContext.chat.dto.MessageRequest;
 import com.example.healthgenie.boundedContext.chat.dto.MessageResponse;
-import com.example.healthgenie.boundedContext.chat.entity.Message;
 import com.example.healthgenie.boundedContext.chat.service.MessageService;
 import com.example.healthgenie.boundedContext.user.entity.User;
 import lombok.RequiredArgsConstructor;
@@ -36,8 +35,8 @@ public class MessageController {
 
     @GetMapping("/chat/{roomId}/messages")
     public ResponseEntity<Result> findAll(@PathVariable Long roomId, @AuthenticationPrincipal User user) {
-        List<Message> response = messageService.findAll(roomId, user);
+        List<MessageResponse> response = MessageResponse.of(messageService.findAll(roomId, user));
 
-        return ResponseEntity.ok(Result.of(MessageResponse.of(response)));
+        return ResponseEntity.ok(Result.of(response));
     }
 }
