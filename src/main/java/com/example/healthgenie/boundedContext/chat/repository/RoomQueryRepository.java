75b15f1af89ebeae2e9a0bc54641ccc5efce77bd
김경렬 <rlarudfuf802@naver.com>
2024-03-15 17:39:28 +0900

    fix: 전체 채팅방 조회 API 수정 - inactive 채팅방이 검색 되던 문제 해결

--- src/main/java/com/example/healthgenie/boundedContext/chat/repository/RoomQueryRepository.java
+++ src/main/java/com/example/healthgenie/boundedContext/chat/repository/RoomQueryRepository.java
@@ -5,16 +5,20 @@ import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.SliceImpl;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Objects;
 
+import static com.example.healthgenie.boundedContext.chat.entity.QRoom.room;
 import static com.example.healthgenie.boundedContext.chat.entity.QRoomUser.roomUser;
 import static com.example.healthgenie.boundedContext.user.entity.QUser.user;
 
+@Slf4j
 @Repository
 @RequiredArgsConstructor
 public class RoomQueryRepository {
@@ -22,37 +26,38 @@ public class RoomQueryRepository {
     private final JPAQueryFactory queryFactory;
 
     public Slice<RoomQueryResponse> findAll(Long userId, Long lastId, Pageable pageable) {
-        List<RoomQueryResponse> contents = queryFactory
-                .select(
-                        Projections.constructor(RoomQueryResponse.class, roomUser.room.id.as("roomId"), user.nickname, user.role, user.profilePhoto)
-                )
-                .from(roomUser)
-                .join(user).on(user.id.eq(roomUser.user.id))
+        List<Long> roomIds = queryFactory
+                .select(room.id)
+                .from(room)
+                .join(roomUser).on(roomUser.room.id.eq(room.id))
                 .where(
                         roomIdLt(lastId),
-                        userIdNe(userId),
-                        roomUserActiveEq(true)
+                        roomUser.user.id.eq(userId),
+                        roomUser.active.eq(true)
                 )
-                .orderBy(roomUser.room.id.desc())
+                .orderBy(room.id.desc())
                 .limit(pageable.getPageSize() + 1)
+                .fetch();
+
+        List<RoomQueryResponse> contents = queryFactory
+                .select(
+                        Projections.constructor(RoomQueryResponse.class,
+                                roomUser.room.id.as("roomId"), user.nickname, user.role, user.profilePhoto)
+                )
+                .from(room)
+                .join(roomUser).on(roomUser.room.id.eq(room.id))
+                .join(user).on(user.id.eq(roomUser.user.id))
+                .where(room.id.in(roomIds), user.id.ne(userId))
                 .fetch();
 
         return checkLastPage(pageable, contents);
     }
 
-    private BooleanExpression roomUserActiveEq(boolean active) {
-        return roomUser.active.eq(active);
-    }
-
-    private BooleanExpression userIdNe(Long userId) {
-        return user.id.ne(userId);
-    }
-
-    private BooleanExpression roomIdLt(Long roomId) {
-        if(roomId == null) {
+    private BooleanExpression roomIdLt(Long lastId) {
+        if(Objects.isNull(lastId)) {
             return null;
         }
-        return roomUser.room.id.lt(roomId);
+        return room.id.lt(lastId);
     }
 
     private Slice<RoomQueryResponse> checkLastPage(Pageable pageable, List<RoomQueryResponse> contents) {
