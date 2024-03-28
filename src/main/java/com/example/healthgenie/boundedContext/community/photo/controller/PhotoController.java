4adef56a5c708441f650162ca0afa7794bf9d554
김경렬 <rlarudfuf802@naver.com>
2024-03-28 15:59:00 +0900

    fix: 잘못된 작업 수정

--- src/main/java/com/example/healthgenie/boundedContext/community/photo/controller/PhotoController.java
+++ src/main/java/com/example/healthgenie/boundedContext/community/photo/controller/PhotoController.java
@@ -1,9 +1,9 @@
-package com.example.healthgenie.boundedContext.trainer.photo.controller;
+package com.example.healthgenie.boundedContext.community.photo.controller;
 
 import com.example.healthgenie.base.response.Result;
-import com.example.healthgenie.boundedContext.trainer.photo.dto.PhotoRequest;
-import com.example.healthgenie.boundedContext.trainer.photo.dto.PhotoResponse;
-import com.example.healthgenie.boundedContext.trainer.photo.service.TrainerProfilePhotoService;
+import com.example.healthgenie.boundedContext.community.photo.dto.PhotoRequest;
+import com.example.healthgenie.boundedContext.community.photo.dto.PhotoResponse;
+import com.example.healthgenie.boundedContext.community.photo.service.PhotoService;
 import com.example.healthgenie.boundedContext.user.entity.User;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -18,36 +18,35 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/trainers/profiles/{profileId}/photos")
+@RequestMapping("/community/posts/{postId}/photos")
 public class PhotoController {
 
-    private final TrainerProfilePhotoService trainerProfilePhotoService;
+    private final PhotoService photoService;
 
     @PostMapping
-    public ResponseEntity<Result> save(@PathVariable Long postId, @AuthenticationPrincipal User user,
-                                       @Valid PhotoRequest request) throws IOException {
-        List<PhotoResponse> response = PhotoResponse.of(trainerProfilePhotoService.save(postId, user.getId(), request));
+    public ResponseEntity<Result> save(@PathVariable Long postId, @AuthenticationPrincipal User user, @Valid PhotoRequest request) throws IOException {
+        List<PhotoResponse> response = PhotoResponse.of(photoService.save(postId, user.getId(), request));
 
         return ResponseEntity.ok(Result.of(response));
     }
 
     @GetMapping("/{photoId}")
     public ResponseEntity<Result> findById(@PathVariable Long postId, @PathVariable Long photoId) {
-        PhotoResponse response = PhotoResponse.of(trainerProfilePhotoService.findById(photoId));
+        PhotoResponse response = PhotoResponse.of(photoService.findById(photoId));
 
         return ResponseEntity.ok(Result.of(response));
     }
 
     @GetMapping
     public ResponseEntity<Result> findAll(@PathVariable Long postId) {
-        List<PhotoResponse> response = PhotoResponse.of(trainerProfilePhotoService.findAllByPostId(postId));
+        List<PhotoResponse> response = PhotoResponse.of(photoService.findAllByPostId(postId));
 
         return ResponseEntity.ok(Result.of(response));
     }
 
     @DeleteMapping
     public ResponseEntity<Result> delete(@PathVariable Long postId, @AuthenticationPrincipal User user) {
-        String response = trainerProfilePhotoService.deleteAllByPostId(postId, user.getId());
+        String response = photoService.deleteAllByPostId(postId, user.getId());
 
         return ResponseEntity.ok(Result.of(response));
     }
