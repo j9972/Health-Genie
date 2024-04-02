32cfc243dca67fb2b0df3964f406337d6acf91de
j9972 <j9972@naver.com>
2024-04-02 13:39:26 +0900

    fix : 충돌 수정 체크용 주석 추카

--- src/main/java/com/example/healthgenie/boundedContext/trainer/photo/controller/ProfilePhotoController.java
+++ src/main/java/com/example/healthgenie/boundedContext/trainer/photo/controller/ProfilePhotoController.java
@@ -19,6 +19,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+//test
 @Slf4j
 @RestController
 @RequiredArgsConstructor
