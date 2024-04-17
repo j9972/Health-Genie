301838ac88eec2aacdc2b1b2b46aa531762006e3
j9972 <j9972@naver.com>
2024-04-17 14:42:27 +0900

    fix : 과거 코드 충돌  삭제

--- src/main/java/com/example/healthgenie/boundedContext/ptrecord/repository/PtProcessPhotoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.healthgenie.boundedContext.ptrecord.repository;
-
-import com.example.healthgenie.boundedContext.ptrecord.entity.PtProcessPhoto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface PtProcessPhotoRepository extends JpaRepository<PtProcessPhoto, Long> {
-
-}
